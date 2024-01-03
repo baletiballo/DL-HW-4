9d9de329b75b6f852ce166a152b5077ee60277ea
baletiballo <75846481+baletiballo@users.noreply.github.com>
2024-01-03 20:21:24 +0100

    normal_form() now works as expected

--- Formula.py
+++ Formula.py
@@ -3,7 +3,7 @@ Formula is essentially an abstract superclass for the recursive syntax-tree (htt
 sub-formulae are the sub-formulae (duh) one for Not and Box; two for And
 applicable_tableaux_rule gives easier access and will allow a match-case construct
 size is the number of logical connectives in the formula. A crude heuristic which branch to choose first
-normal_form takes care of Diamond, or, -> and alike. Please use it when parsing formulae.
+normal_form takes care of Diamond, or, -> and alike. Please use it after parsing formulae.
 
 Formula implements __eq__ thus comparisons with == (or "in") are possible
 """
@@ -52,10 +52,11 @@ class Not(Formula):
             self.applicable_tableaux_rule = None
 
     def normal_form(self):  # remove double negation at parse-time
+        self.sub_formulae[0] = self.sub_formulae[0].normal_form()
         if isinstance(self.sub_formulae[0], Not):
-            return self.sub_formulae[0].sub_formulea[0].normal_form()
+            return self.sub_formulae[0].sub_formulae[0]  # this has already been normalized
         else:  # normal form may create instances of "NotAnd" or "NotBox" rules, creating a new object detects that
-            return Not(self.sub_formulae[0].normal_form())
+            return Not(self.sub_formulae[0])
 
     def __str__(self):
         return f"\u00AC {self.sub_formulae[0]!s}"
@@ -73,7 +74,7 @@ class And(Formula):
         return self
 
     def __str__(self):
-        return f"({self.sub_formulae[0]!s}) \u2227 ({self.sub_formulae[1]!s})"
+        return f"({self.sub_formulae[0]!s} \u2227 {self.sub_formulae[1]!s})"
 
 
 class Box(Formula):
@@ -81,6 +82,10 @@ class Box(Formula):
         super().__init__()
         self.sub_formulae = [boxed]
         self.applicable_tableaux_rule = None
+
+    def normal_form(self):
+        self.sub_formulae[0] = self.sub_formulae[0].normal_form()
+        return self
 
     def __str__(self):
         return f"\u25FB {self.sub_formulae[0]!s}"
@@ -93,7 +98,7 @@ class Or(Formula):
         self.applicable_tableaux_rule = None
 
     def normal_form(self):  # A \/ B = ~(~A /\ ~B)
-        return Not(And(Not(self.sub_formulae[0]), Not(self.sub_formulae[1])))
+        return Not(And(Not(self.sub_formulae[0]), Not(self.sub_formulae[1]))).normal_form()
 
     def __str__(self):
         return f"{self.sub_formulae[0]!s} \u2228 {self.sub_formulae[1]!s}"
@@ -106,7 +111,7 @@ class Implication(Formula):
         self.applicable_tableaux_rule = None
 
     def normal_form(self):  # A -> B = ~A \/ B = ~(~(~A) /\ ~B = ~(A /\ ~B)
-        return Not(And(self.sub_formulae[0], Not(self.sub_formulae[1])))
+        return Not(And(self.sub_formulae[0], Not(self.sub_formulae[1]))).normal_form()
 
     def __str__(self):
         return f"{self.sub_formulae[0]!s} \u2192 {self.sub_formulae[1]!s}"
@@ -120,7 +125,7 @@ class BiImplication(Formula):
 
     def normal_form(self):  # A <-> B = A -> B /\ B -> A   see above
         return And(Not(And(self.sub_formulae[0], Not(self.sub_formulae[1]))),
-                   Not(And(self.sub_formulae[1], Not(self.sub_formulae[0]))))
+                   Not(And(self.sub_formulae[1], Not(self.sub_formulae[0])))).normal_form()
 
     def __str__(self):
         return f"{self.sub_formulae[0]!s} \u21D4 {self.sub_formulae[1]!s}"
@@ -133,7 +138,7 @@ class Diamond(Formula):
         self.applicable_tableaux_rule = None
 
     def normal_form(self):  # <> A = ~[]~A
-        return Not(Box(Not(self.sub_formulae[0])))
+        return Not(Box(Not(self.sub_formulae[0]))).normal_form()
 
     def __str__(self):
         return f"\u25C7 {self.sub_formulae[0]!s}"
